3303594b3cb5d546b72b7f314f0ab08ad9c03c54
Welson Rosendo Rodrigues <164583871+welson-rodrigues@users.noreply.github.com>
2026-01-27 23:41:51 -0300

    Remove comments and clean up exit statements

--- internet.sh
+++ internet.sh
@@ -8,12 +8,10 @@ if [ -z "$username" ] || [ -z "$password" ]; then
     exit 1
 fi
 
-# Curl silencioso (-s) ignorando SSL (-k)
 curl -k -s -d "escapeUser=$username&user=$username&passwd=$password&ok=Login" -X POST "https://autenticacao-cn.ifrn.local:6082/php/uid.php?vsys=1&rule=0" > /dev/null
 
-# Teste de conexão
 if ping -c 1 8.8.8.8 > /dev/null; then
-    exit 0 # Sucesso
+    exit 0 
 else
-    exit 1 # Falha
+    exit 1 
 fi
